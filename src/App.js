dc274f069b0da80e71e346935aceefd09b49a34e
Ben Tupper <Ben@Bens-MacBook-Pro-4.local>
2017-12-03 11:52:48 +0000

    ravenous

--- src/App.js
+++ src/App.js
@@ -2,36 +2,31 @@ import React, { Component } from 'react';
 import './App.css';
 import BusinessList from './components/BusinessList/BusinessList.js';
 import SearchBar from './components/SearchBar/SearchBar.js';
+import Yelp from './util/Yelp.js'
+// eslint-disable-next-line
 import logo from './logo.svg';
 
-const business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      businesses: []
+    };
+    this.searchYelp = this.searchYelp.bind(this);
+  }
 
-const businesses = [
-  business,
-  business,
-  business,
-  business,
-  business,
-  business
-];
+  searchYelp(term, location, sortBy) {
+    Yelp.search(term, location, sortBy).then(businesses => {
+      this.setState({businesses: businesses});
+    });
+  }
 
-class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
-        <SearchBar />
-        <BusinessList businesses={businesses} />
+        <SearchBar searchYelp={this.searchYelp}/>
+        <BusinessList businesses={this.state.businesses} />
       </div>
     );
   }
--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -5,14 +5,13 @@ import Business from '../Business/Business';
 class BusinessList extends React.Component {
   render() {
     return (
-    <div className="BusinessList">
-      {this.props.businesses.map(business => {
-        <Business business={business} />
-      })}
+      <div className="BusinessList">
+        {this.props.businesses.map(business => {
+          return <Business business={business} key={business.id} />
+        })}
       </div>
     )
   }
 }
-
 
 export default BusinessList;
